871e4c17556a474ad9f2231665a95e6e270105b9
NelisVerhoef <verhoefnelis@gmail.com>
2018-11-09 13:43:57 +1000

    Update config.py

--- config.py
+++ config.py
@@ -65,14 +65,14 @@ DB_AUTO_UPGRADE = False
 # used for that class below.
 # By default, the knowledge repo offers:
 # ['debug', 'oauth2', 'bitbucket', 'github', 'google']
-AUTH_PROVIDERS = []
+AUTH_PROVIDERS = ['github']
 
 # If you are going to use a OAuth provider, you will need to specify client ids
 # and private tokens. This can be done by instantiating instances of
 # `OAuth2Provider` and adding them to the above list, or by specifying OAuth
 # connection properties as demonstrated below for the GitHub authenticator.
-# OAUTH_GITHUB_CLIENT_ID = '<client id>'
-# OAUTH_GITHUB_CLIENT_SECRET = '<client secret>'
+OAUTH_GITHUB_CLIENT_ID = '03fe495a35ab1ec39395'
+OAUTH_GITHUB_CLIENT_SECRET = '5188b3674e29410e86c40cd2a757575e78b055d5'
 
 # To configure a generic OAuth provider that is not one of the presets
 # provided, you may use the provider 'oauth2' which creates an empty,
